2fb473be25512a48f0867bbda6001666db7a9c40
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-04-24 16:56:18 +0800

    Add HashWithOpenAddressing (linear probing)

--- hashing.py
+++ hashing.py
@@ -1,3 +1,5 @@
+import sys
+
 class HashWithChaining:
   def __init__(self, size=10000):
     self.filled_size = 0
@@ -44,7 +46,9 @@ class HashWithChaining:
     matched_key_value_pair = next((key_value_pair for key_value_pair
                                    in self.hash_indices[hashed_key]
                                    if key_value_pair[0] == key), None)
-    return matched_key_value_pair
+
+    if matched_key_value_pair == None: return None
+    return matched_key_value_pair[1]
 
   def remove(self, key):
     hashed_key = self.__hash(key)
@@ -65,3 +69,57 @@ class HashWithChaining:
 
         self.size = self.size // 2
         self.hash_indices = new_table_doubling.hash_indices
+
+class HashWithOpenAddressing:
+  DELETED_FLAG = sys.maxsize
+
+  def __init__(self, size=10000):
+    self.filled_size = 0
+    self.size = size
+    self.hash_values = [None for _ in range(0, self.size)]
+
+  @staticmethod
+  def __pre_hash(key):
+    return key.__hash__()
+
+  def __hash(self, key, counter):
+    pre_hashed_key = HashWithOpenAddressing.__pre_hash(key)
+
+    random_1 = 17933623
+    random_2 = 86536918
+    large_prime = 104395301
+    return (((random_1 * pre_hashed_key + random_2) % large_prime) + counter) % self.size
+
+  def put(self, key, value):
+    for i in range(0, self.size):
+      hashed_key = self.__hash(key, i)
+      if self.hash_values[hashed_key] == None or self.hash_values[hashed_key] == HashWithOpenAddressing.DELETED_FLAG:
+        self.hash_values[hashed_key] = (key, value)
+        self.filled_size += 1
+        return
+      elif self.hash_values[hashed_key][0] == key:
+        # overwrite
+        self.hash_values[hashed_key] = (key, value)
+        return
+
+    raise Exception("Hash full")
+
+  def get(self, key):
+    for i in range(0, self.size):
+      hashed_key = self.__hash(key, i)
+      if self.hash_values[hashed_key] == HashWithOpenAddressing.DELETED_FLAG: continue
+      if self.hash_values[hashed_key] == None: return None
+      elif self.hash_values[hashed_key][0] == key:
+        return self.hash_values[hashed_key][1]
+
+    return None
+
+  def remove(self, key):
+    for i in range(0, self.size):
+      hashed_key = self.__hash(key, i)
+      if self.hash_values[hashed_key] == HashWithOpenAddressing.DELETED_FLAG: continue
+      if self.hash_values[hashed_key] == None: return
+      elif self.hash_values[hashed_key][0] == key:
+        self.hash_values[hashed_key] = HashWithOpenAddressing.DELETED_FLAG
+        self.filled_size -= 1
+        return
--- hashing_test.py
+++ hashing_test.py
@@ -2,16 +2,16 @@ import unittest
 from hashing import *
 
 class TestSort(unittest.TestCase):
-  def test_put_and_get(self):
+  def test_hash_with_chaining(self):
     hash = HashWithChaining(2)
     self.assertEqual(hash.get(1), None)
 
     hash.put(1, "one")
     hash.put(1, "uno")
-    self.assertEqual(hash.get(1), (1, "uno"))
+    self.assertEqual(hash.get(1), "uno")
 
     hash.put(1, "satu")
-    self.assertEqual(hash.get(1), (1, "satu"))
+    self.assertEqual(hash.get(1), "satu")
 
     hash.put(2, "dua")
     hash.put(2, "two")
@@ -25,9 +25,9 @@ class TestSort(unittest.TestCase):
     hash.put(9, "nine")
     hash.put(10, "ten")
 
-    self.assertEqual(hash.get(2), (2, "two"))
-    self.assertEqual(hash.get(3), (3, "san"))
-    self.assertNotEqual(hash.get(3), (3, "three"))
+    self.assertEqual(hash.get(2), "two")
+    self.assertEqual(hash.get(3), "san")
+    self.assertNotEqual(hash.get(3), "three")
 
     self.assertEqual(hash.filled_size, 10)
     self.assertEqual(hash.size, 16)
@@ -46,6 +46,42 @@ class TestSort(unittest.TestCase):
     hash.remove(9)
     self.assertEqual(hash.filled_size, 4)
     self.assertEqual(hash.size, 8)
+
+  def test_hash_with_open_addressing(self):
+    hash = HashWithOpenAddressing(4)
+    self.assertEqual(hash.size, 4)
+
+    hash.put(1, "one")
+    hash.put(1, "uno")
+    self.assertEqual(hash.get(1), "uno")
+    self.assertEqual(hash.filled_size, 1)
+
+    hash.put(2, "two")
+    self.assertEqual(hash.get(2), "two")
+    self.assertEqual(hash.filled_size, 2)
+
+    hash.remove(1)
+    self.assertEqual(hash.get(1), None)
+    self.assertEqual(hash.filled_size, 1)
+
+    hash.remove(23)
+    self.assertEqual(hash.filled_size, 1)
+
+    hash.put(1, "one")
+    self.assertEqual(hash.get(1), "one")
+    self.assertEqual(hash.filled_size, 2)
+
+    hash.put(3, "three")
+    hash.put(4, "four")
+    with self.assertRaises(Exception):
+      hash.put(5, "five")
+
+  def test_hash_with_open_addressing_probing(self):
+    hash_1 = HashWithOpenAddressing(100)
+    for i in range(0,100):
+      hash_1.put(i*2, i)
+    with self.assertRaises(Exception):
+      hash_1.put(1, "helo")
 
 if __name__ == "__main__":
   unittest.main()
