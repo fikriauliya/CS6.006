b68ee0deb42ee1958e91da2138e891844d99ab33
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-04-25 15:21:57 +0800

    Add multiply and karatsuba_multiply

--- integer_arithmetic.py
+++ integer_arithmetic.py
@@ -1,3 +1,5 @@
+import math
+
 def catalan_number_count_rec(number_of_parantheses_pairs):
   """
   0: empty
@@ -30,3 +32,63 @@ def catalan_number_count_iterative(number_of_parantheses_pairs):
       catalans[i] += catalans[j] * catalans[i - j - 1]
 
   return catalans[number_of_parantheses_pairs]
+
+def square_root(number, precision = 4):
+  prev_iteration_result = 1
+  for i in range(precision):
+    cur_iteration_result = (prev_iteration_result + number/prev_iteration_result) / 2
+    prev_iteration_result = cur_iteration_result
+
+  return prev_iteration_result
+
+def multiply(x, y):
+  if x == 0 or y == 0: return 0
+  sign = 1
+  if (x < 0 and y > 0) or (x > 0 and y < 0): sign = -1
+  x = abs(x)
+  y = abs(y)
+  max_digit = max(x.bit_length(), y.bit_length())
+
+  if max_digit == 1: return 1
+
+  half_digit = max_digit // 2
+  filter_mask = (1 << half_digit) - 1
+
+  x_upper_half = x >> half_digit
+  x_lower_half = x & filter_mask
+  y_upper_half = y >> half_digit
+  y_lower_half = y & filter_mask
+
+  abs_res = ((multiply(x_upper_half, y_upper_half)  << half_digit << half_digit) +
+          ((multiply(x_lower_half, y_upper_half) + multiply(x_upper_half, y_lower_half)) << half_digit) +
+          (multiply(x_lower_half, y_lower_half)))
+  if sign == -1: return ~abs_res + 1
+  else: return abs_res
+
+def karatsuba_multiply(x, y):
+  if x == 0 or y == 0: return 0
+  sign = 1
+  if (x < 0 and y > 0) or (x > 0 and y < 0): sign = -1
+  x = abs(x)
+  y = abs(y)
+  max_digit = max(x.bit_length(), y.bit_length())
+
+  if max_digit == 1: return 1
+
+  half_digit = max_digit // 2
+  filter_mask = (1 << half_digit) - 1
+
+  x_upper_half = x >> half_digit
+  x_lower_half = x & filter_mask
+  y_upper_half = y >> half_digit
+  y_lower_half = y & filter_mask
+
+  z_0 = karatsuba_multiply(x_lower_half, y_lower_half)
+  z_2 = karatsuba_multiply(x_upper_half, y_upper_half)
+  z_1 = karatsuba_multiply((x_lower_half + x_upper_half), (y_lower_half + y_upper_half)) - z_0 - z_2
+
+  abs_res = ((z_2  << half_digit << half_digit) +
+          (z_1 << half_digit) +
+          z_0)
+  if sign == -1: return ~abs_res + 1
+  else: return abs_res
--- integer_arithmetic_test.py
+++ integer_arithmetic_test.py
@@ -1,5 +1,7 @@
 import unittest
-from integer_arithmetic import catalan_number_count_iterative, catalan_number_count_rec
+import math
+
+from integer_arithmetic import *
 
 class CatalanNumberTest(unittest.TestCase):
   def test_catalan_number(self):
@@ -10,6 +12,35 @@ class CatalanNumberTest(unittest.TestCase):
 
     for i in range(0, 10):
       self.assertEqual(catalan_number_count_iterative(i), catalan_number_count_rec(i))
+
+class CommonArithmeticTest(unittest.TestCase):
+  def test_square_root(self):
+    self.assertEqual(square_root(2, 0), 1)
+    self.assertEqual(square_root(2, 1), 1.5)
+    self.assertEqual(square_root(2, 10), square_root(2, 11))
+    self.assertEqual(round(square_root(2, 10), 10), round(math.sqrt(2), 10))
+
+  def test_multiply(self):
+    self.assertEqual(multiply(2, 4), 8)
+    self.assertEqual(multiply(3, 3), 9)
+    self.assertEqual(karatsuba_multiply(-3, -3), 9)
+    self.assertEqual(multiply(11, 11), 121)
+    self.assertEqual(multiply(121, 11), 11*121)
+    self.assertEqual(multiply(12100, 11), 11*12100)
+    self.assertEqual(multiply(12100, 2), 2*12100)
+    self.assertEqual(multiply(12100, -2), -2*12100)
+    self.assertEqual(multiply(-12100, -2), -2*-12100)
+
+  def test_karatsuba_multiply(self):
+    self.assertEqual(karatsuba_multiply(2, 4), 8)
+    self.assertEqual(karatsuba_multiply(3, 3), 9)
+    self.assertEqual(karatsuba_multiply(-3, -3), 9)
+    self.assertEqual(karatsuba_multiply(11, 11), 121)
+    self.assertEqual(karatsuba_multiply(121, 11), 11*121)
+    self.assertEqual(karatsuba_multiply(12100, 11), 11*12100)
+    self.assertEqual(karatsuba_multiply(12100, 2), 2*12100)
+    self.assertEqual(karatsuba_multiply(12100, -2), -2*12100)
+    self.assertEqual(karatsuba_multiply(-12100, -2), -2*-12100)
 
 if __name__ == "__main__":
   unittest.main()
