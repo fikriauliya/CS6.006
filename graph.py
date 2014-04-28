075956685940ccdde9f43fa4df492adb8e90df0b
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-04-28 15:55:53 +0800

    Add DFS, categorize edge, & topological sort

--- graph.py
+++ graph.py
@@ -1,24 +1,31 @@
+import itertools
+
 class Graph:
-  adj_list = {}
+  class EdgesType:
+    FORWARD = "F"
+    CROSS = "C"
+    BACK = "B"
+    UNMARKED = "U"
 
   def __init__(self, graph_ascii = ''):
-    edges = graph_ascii.split("\n")
+    self.adj_list = {}
 
-    source_vertex_counter = 0
+    edges = graph_ascii.split("\n")
+    source_vertex = 0
     for edge in edges:
-      destination_vertex_counter = 0
-      self.adj_list[source_vertex_counter] = set()
-      for status in edge:
-        if status == '1':
-          self.adj_list[source_vertex_counter].add(destination_vertex_counter)
-        destination_vertex_counter += 1
-      source_vertex_counter += 1
+      destination_vertex = 0
+      self.adj_list[source_vertex] = []
+      for bit in edge:
+        if bit == '1':
+          self.adj_list[source_vertex].append((destination_vertex, Graph.EdgesType.UNMARKED))
+        destination_vertex += 1
+      source_vertex += 1
 
   def print(self):
     for v1 in self.adj_list:
-      print(v1, end="->")
-      for v2 in self.adj_list[v1]:
-        print(v2, end=" ")
+      print(chr(v1 + ord('A')), end="->")
+      for (v2, et) in self.adj_list[v1]:
+        print(chr(v2 + ord('A')) + '(' + et + ')', end=" ")
       print()
 
   # O(E)
@@ -31,7 +38,7 @@ class Graph:
     while frontiers:
       next_frontiers = set()
       for frontier in frontiers:
-        for adj_vertex in self.adj_list[frontier]:
+        for (adj_vertex, _) in self.adj_list[frontier]:
           if not adj_vertex in level:
             level[adj_vertex] = cur_level
             parent[adj_vertex] = frontier
@@ -40,11 +47,80 @@ class Graph:
       frontiers = next_frontiers
       cur_level += 1
 
-    return level
+    return (parent, level)
 
   def shortest_path(self, start, destination):
-    level = self.bfs(start)
+    (parent, level) = self.bfs(start)
     if destination in level:
       return level[destination]
     else:
       return None
+
+  # O(E)
+  def dfs(self, start = 0):
+    level = {start: 0}
+    parent = {start: -1}
+
+    def visit(start, cur_level):
+      for (v, _) in self.adj_list[start]:
+        if not v in parent:
+          parent[v] = start
+          level[v] = cur_level
+          visit(v, cur_level + 1)
+
+    visit(start, 1)
+    return (parent, level)
+
+  # O(E + V)
+  def categorize_edges(self):
+    parent = {}
+    cur_parent = {}
+    cur_stack = set()
+
+    def visit(start):
+      for i in range(len(self.adj_list[start])):
+        v = self.adj_list[start][i][0]
+        if v in parent:
+          self.adj_list[start][i] = (v, Graph.EdgesType.CROSS)
+        elif not v in cur_stack:
+          cur_parent[v] = start
+          self.adj_list[start][i] = (v, Graph.EdgesType.FORWARD)
+          cur_stack.add(start)
+          visit(v)
+          cur_stack.remove(start)
+        else:
+          self.adj_list[start][i] = (v, Graph.EdgesType.BACK)
+
+    for s in self.adj_list:
+      if not s in parent:
+        parent[s] = -1
+        cur_parent[s] = -1
+        visit(s)
+
+        parent = dict(itertools.chain(parent.items(), cur_parent.items()))
+
+    print("Parent", parent)
+
+  # O(E + V)
+  def topological_sort(self):
+    res = []
+    cur_traversal_path = set()
+    visited = set()
+
+    def visit(start):
+      for v, _ in self.adj_list[start]:
+        if v in cur_traversal_path:
+          raise Exception("Cyclic!")
+        if not v in visited:
+          cur_traversal_path.add(v)
+          visit(v)
+          cur_traversal_path.remove(v)
+
+      visited.add(start)
+      res.insert(0, start)
+
+    for s in self.adj_list:
+      if not s in visited:
+        visit(s)
+
+    return res
--- graph_test.py
+++ graph_test.py
@@ -1,4 +1,6 @@
+from operator import methodcaller
 import unittest
+import operator
 from graph import *
 
 class GraphTest(unittest.TestCase):
@@ -12,7 +14,7 @@ class GraphTest(unittest.TestCase):
     g = Graph('1110\n'
               '0110\n'
               '0011\n'
-              '1111\n')
+              '1111')
     g.print()
     g.bfs()
 
@@ -21,7 +23,7 @@ class GraphTest(unittest.TestCase):
               '01100\n'
               '00110\n'
               '11110\n'
-              '00000\n')
+              '00000')
     self.assertEqual(g.shortest_path(0, 0), 0)
     self.assertEqual(g.shortest_path(0, 1), 1)
     self.assertEqual(g.shortest_path(0, 2), 1)
@@ -30,3 +32,35 @@ class GraphTest(unittest.TestCase):
     self.assertEqual(g.shortest_path(2, 1), 2)
     self.assertEqual(g.shortest_path(2, 4), None)
 
+  def test_graph_dfs(self):
+    g = Graph('11100\n'
+              '01100\n'
+              '00110\n'
+              '11111\n'
+              '00000')
+    g.print()
+    (parent, level) = g.dfs()
+    print("Parent", parent)
+    sorted_level = sorted(level.items(),key=operator.itemgetter(1))
+    print("Level", sorted_level)
+
+  def test_categorize_edges(self):
+    g = Graph('010100\n'
+              '000010\n'
+              '000011\n'
+              '010000\n'
+              '000100\n'
+              '000001')
+    g.categorize_edges()
+    g.print()
+
+  def test_topological_sort(self):
+    g = Graph('001\n'
+              '100\n'
+              '000')
+    g.print()
+    res = g.topological_sort()
+    print(res)
+
+if __name__ == "__main__":
+  unittest.main()
